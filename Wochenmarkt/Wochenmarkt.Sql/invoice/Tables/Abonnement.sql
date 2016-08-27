07329272b4ab9bf65689d91b19b471279f4b44e5
NightkingCH <thomas.erni@nk-s.ch>
2016-08-27 18:25:39 +0200

    Add constraints

--- Wochenmarkt/Wochenmarkt.Sql/invoice/Tables/Abonnement.sql
+++ Wochenmarkt/Wochenmarkt.Sql/invoice/Tables/Abonnement.sql
@@ -7,5 +7,7 @@
 	[GueltigBis] DATETIME2(7) NOT NULL,
 	CONSTRAINT PK_AbonnementId PRIMARY KEY ([AbonnementId]),
 	CONSTRAINT FK_Abonnement_Abonnement_AbonnementTyp FOREIGN KEY ([AbonnementTypId]) REFERENCES [invoice].[AbonnementTyp] ([AbonnementTypId]),
-	CONSTRAINT FK_Abonnement_Abonnement_Standbetreiber FOREIGN KEY ([StandbetreiberId]) REFERENCES [app].[Standbetreiber] ([StandbetreiberId])
+	CONSTRAINT FK_Abonnement_Abonnement_Standbetreiber FOREIGN KEY ([StandbetreiberId]) REFERENCES [app].[Standbetreiber] ([StandbetreiberId]),
+	CONSTRAINT CK_Abonnement_GueltigAbLessThanGueltigBis CHECK ([GueltigAb] <= [GueltigBis]),
+	CONSTRAINT CK_Abonnement_GueltigBisGreaterThanGueltigAb CHECK ([GueltigBis] >= [GueltigAb])
 )
--- Wochenmarkt/Wochenmarkt.Sql/invoice/Tables/Miete.sql
+++ Wochenmarkt/Wochenmarkt.Sql/invoice/Tables/Miete.sql
@@ -5,5 +5,6 @@
 	[Bezahlt] BIT NOT NULL DEFAULT(0),
 	[Mahnstufe] INT NOT NULL DEFAULT(0), -- Mahnwesen, 0 = Keine Mahnung, 1 = 1. Mahnung, 2 = 2. Mahnung, 3 = 3. Mahnung, 4 = Betreibung
 	[GemahntAm] DATETIME2(7) NULL,
-	CONSTRAINT PK_Miete_MieteId PRIMARY KEY ([MieteId])
+	CONSTRAINT PK_Miete_MieteId PRIMARY KEY ([MieteId]),
+	CONSTRAINT CK_Miete_GreaterZero CHECK ([Betrag] > 0 )
 )
