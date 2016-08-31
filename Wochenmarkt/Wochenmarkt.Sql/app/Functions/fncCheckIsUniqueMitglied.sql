79ca2721e4f09b19c0e561dad5eceff5a06693a1
Thomas Erni <thomas.erni@advellence.com>
2016-08-31 15:42:46 +0200

    Fixed check constraint.

--- Wochenmarkt/Wochenmarkt.Sql/app/Functions/fncCheckIsUniqueMitglied.sql
+++ Wochenmarkt/Wochenmarkt.Sql/app/Functions/fncCheckIsUniqueMitglied.sql
@@ -2,19 +2,20 @@
     @vorname NVARCHAR(MAX)
 	, @nachname NVARCHAR(MAX)
 	, @adresseId INT
-	, @geburtsdatum NVARCHAR(MAX)
+	, @geburtsdatum DATETIME2(7)
 )
 RETURNS BIT
 AS
 BEGIN
-    IF EXISTS (SELECT TOP 1 
-					mt.[Vorname] 
+	-- we have to use count because when the check fires, the data record is already inserted. therfore it would raise always an error!
+    IF (SELECT
+					COUNT(mt.[Vorname])
 				FROM [app].Mitglied mt
 				WHERE mt.[Vorname] = @vorname
 						AND mt.[Nachname] = @nachname
 						AND mt.[AdresseId] = @adresseId
-						AND mt.[Geburtsdatum]= @geburtsdatum
-				)
+						AND DATEADD(day, 0, mt.[Geburtsdatum]) = CAST(DATEADD(day, 0, @geburtsdatum) AS DATETIME2(7))
+				) >= 2
 		BEGIN
 			RETURN CAST(0 AS BIT)
 		END
