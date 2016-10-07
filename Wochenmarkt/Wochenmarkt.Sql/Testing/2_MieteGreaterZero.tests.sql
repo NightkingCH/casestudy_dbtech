f3c3755f40e0c9587c0e3b1aad694fcb63321c97
NightkingCH <thomas.erni@nk-s.ch>
2016-10-07 12:52:52 +0200

    Remove a redudant test case

--- Wochenmarkt/Wochenmarkt.Sql/Testing/2_MieteGreaterZero.tests.sql
+++ Wochenmarkt/Wochenmarkt.Sql/Testing/2_MieteGreaterZero.tests.sql
@@ -3,19 +3,7 @@ GO
 
 BEGIN TRANSACTION TH_Miete
 
-	--TC:2.1 Betrag > 0 --> möglich
-	INSERT INTO [invoice].[Miete]
-			   (
-				   [Betrag] 
-				   ,[Bezahlt]
-				   ,[Mahnstufe]
-				   ,[GemahntAm]
-			   )
-		 VALUES
-			   (100.00 ,0 ,0 ,NULL)
-	GO
-
-	--TC:2.2 Betrag > 0 und mit Dezimal --> möglich
+	--TC:2.1 Betrag > 0 und mit Dezimal --> möglich
 	INSERT INTO [invoice].[Miete]
 			   (
 				   [Betrag] 
@@ -27,7 +15,7 @@ BEGIN TRANSACTION TH_Miete
 			   (100.87545 ,0 ,0 ,NULL)
 	GO
 
-	--TC:2.3 Betrag = 0 --> nicht möglich
+	--TC:2.2 Betrag = 0 --> nicht möglich
 	INSERT INTO [invoice].[Miete]
 			   (
 				   [Betrag] 
@@ -39,7 +27,7 @@ BEGIN TRANSACTION TH_Miete
 			   (0 ,0 ,0 ,NULL)
 	GO
 
-	--TC:2.4 Betrag < 0 --> nicht möglich
+	--TC:2.3 Betrag < 0 --> nicht möglich
 	INSERT INTO [invoice].[Miete]
 			   (
 				   [Betrag] 
